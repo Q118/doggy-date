fd011e9ffd01fa4148bfbb7201aeee0988c50bc4
Q118 <shelbyfish91@gmail.com>
2021-02-15 10:48:04 -0800

    swiping functionality in place, no images set yet

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,6 +2,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const apolloClient = new ApolloClient({
-  uri: 'https://puppy-playdate.us-west-2.aws.cloud.dgraph.io/graphql',
+  uri: 'https://doggy-date.us-west-2.aws.cloud.dgraph.io/graphql',
   cache: new InMemoryCache(),
 })
