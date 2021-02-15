98c38724804f8a9a3740e9783bfd1ad0b6fd0abc
Q118 <shelbyfish91@gmail.com>
2021-02-15 10:38:00 -0800

    rendering welcome page on Loan

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,5 +1,7 @@
-// first intiialize a new client and then wrap your root component with a provider component
+  
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
 export const apolloClient = new ApolloClient({
-    uri: 'https://doggy-date.us-west-2.aws.cloud.dgraph.io/graphql',
-    cache: new InMemoryCache(),
+  uri: 'https://puppy-playdate.us-west-2.aws.cloud.dgraph.io/graphql',
+  cache: new InMemoryCache(),
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 /** @format */
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
+
+import "./index.css";
+import App from "./App";
+import { apolloClient } from "./apolloClient";
+import * as serviceWorker from "./serviceWorker";
+
 ReactDOM.render(
 	<React.StrictMode>
 		<ApolloProvider client={apolloClient}>
@@ -8,3 +17,8 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
